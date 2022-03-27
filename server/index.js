191c942aec767be4feee645485363d8c957a9f18
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-03-27 17:05:37 -0300

    feat: configure server

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io');
+
+const app = express();
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+server.listen(3001, () => {
+  console.log('Server running on port 3001');
+});
