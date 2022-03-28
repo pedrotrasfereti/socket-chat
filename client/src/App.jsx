bb0e5880c25085c1a3e63394fa6ded3cba4c0009
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-03-27 22:36:06 -0300

    feat: create placeholder event

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
+import io from 'socket.io-client';
+
+const socket = io.connect('https://localhost/3001');
 
 function App() {
+  const sendMessage = () => {
+    socket.emit(
+      'send_message',
+      { message: 'Hello' },
+    );
+  };
+
+  useEffect(() => {
+    socket.on('receive_message', (data) => {
+      alert(data.message);
+    });
+  }, [socket]);
+
   return (
-    <div className="App"></div>
+    <div className='App'>
+      <input className='Input' placeholder='Message...' />
+      <button onClick={ () => sendMessage }>
+        Send Message
+      </button>
+    </div>
   )
 };
 
